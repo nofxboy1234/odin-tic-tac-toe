8885db3794f322b3e9217d46acd37e4234a0bd4e
Dylan Palmboom <nofxboy1234@gmail.com>
2022-09-04 12:29:04 +0200

    Add game board, controller and player objects

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
-function name(params) {
-  let a = 1;
-}
+const gameBoard = (() => {
+  const squares = [];
+})();
+
+const gameController = (() => {
+  const turns = 0;
+})();
+
+const player = (name) => {
+  return {
+    name,
+  };
+};
+
+const player1 = player('Player 1');
+const player2 = player('Player 2');
+console.log(player1.name);
+console.log(player2.name);
