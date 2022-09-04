646f1c6b988fdf0f36819a1eefad0d4404b30715
Dylan Palmboom <nofxboy1234@gmail.com>
2022-09-04 13:16:14 +0200

    Start displayController.render method

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const gameBoard = (() => {
   const squares = ['X', 'X', 'X', 'O', 'O', 'O', 'X', 'X', 'X'];
+
+  return {
+    squares,
+  };
 })();
 
 const gameController = (() => {
@@ -8,7 +12,9 @@ const gameController = (() => {
 
 const displaycController = (() => {
   let render = () => {
-    console.log('render');
+    gameBoard.squares.forEach((element) => {
+      console.log(element);
+    });
   };
 
   return {
@@ -26,3 +32,5 @@ const player1 = player('Player 1');
 const player2 = player('Player 2');
 console.log(player1.name);
 console.log(player2.name);
+
+displaycController.render();
